d3ec97735a357bb4fa8bf7fb0856c377ede600d3
kaiawal <kaia.walker@gmail.com>
2025-05-05 12:47:16 -0700

    testing

--- node_webpage/app.js
+++ node_webpage/app.js
@@ -9,13 +9,76 @@ const PORT = 53005;     // Set a port number
 
 // Database 
 const db = require('./db-connector');
+app.use(express.json());
 
 /*
     ROUTES
 */
 
-app.get('/', async function (req, res) {
+app.post('/', async function (req, res) {
+try {
+    const { userID, gameID } = req.body;
+    
+    if (!userID || !gameID) {
+        return res.status(400).send("user_id or game_id missing from req.body");
+    }
 
+    const query = 'INSERT INTO userLibrary (userID, gameID) VALUES (' + userID + ', ' + gameID + ');';
+
+    await db.query(query);
+
+    res.status(200).send("Game successfully added to userLibrary");
+} catch {
+    console.error("Error adding game to library", error);
+    res.status(500).send('Error adding game to library');
+}
+}); 
+
+/*
+For filtering:
+
+
+filterString:
+    NULL- filter does not exist
+    genres.name - filter for genre
+    platforms.name - filter for platform
+    games.numUsers- filter for numUsers
+    games.rating- filter for rating
+    anything else- throw an error
+
+filter:
+    the thing to be filtered through sliders/inputs
+    ex: filterString = genres.name, filter = 'rpg'
+*/
+
+/* Still to implement:
+.get('/games:id')
+.get('/userLibrary')
+*/
+
+app.get('/dasboard', async function (req, res) {
+    try {
+        const {filterString, filter} = req.body;
+
+        if (!filterString) {
+            const query1 = `SELECT games.gameID, games.name AS name, genres.name AS genre, 
+                                platforms.name AS platform, games.numUsers, 
+                                games.rating, games.description '
+                        FROM games 
+                        INNER JOIN genres ON genres.genreID = games.genreID 
+                        INNER JOIN platforms ON platforms.platformID = games.platformID 
+                        ORDER BY games.name DESC`;
+        } 
+        const games = await db.query(query);
+        if (!games) {
+            res.status(400).send("Error fetching games");
+        }
+        res.status(200).json(games);
+
+    } catch (error) {
+        console.error("Error fetching games", error);
+        res.status(500).send("Error occured fetching games.");
+    }
     //try {
         
         // Define our queries
@@ -41,7 +104,6 @@ app.get('/', async function (req, res) {
         // Send a generic error message to the browser
         //res.status(500).send("An error occurred while executing the database queries.");
     //}
-    res.sendFile(__dirname + '/public/index.html');
 });
 
 /*
